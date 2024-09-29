f9f357052546301e8ca4a3bfd06265217f65f605
root <root@THINKPAD-E9D5J71Q>
2024-09-29 14:52:11 -0400

    Boot.dev tutorial

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 import pygame
-
+import sys
 from constants import *
-
+from player import Player
+from asteroid import Asteroid
+from shot import Shot
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    af = AsteroidField()
+    dt = 0
+
     while True:
-        color = (0,0,0)
-        screen.fill(color)
-        pygame.display.flip()
-	
-	for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for ast in asteroids:
+            if player.check_overlap(ast):
+                sys.exit()
+            for s in shots:
+                if ast.check_overlap(s):
+                    s.kill()
+                    ast.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
